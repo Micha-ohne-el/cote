a224a834816b1cb146012a19d5720f2ff8fdb9e7
Micha Lehmann <michalehmann0112@gmail.com>
2024-04-03 15:32:24 +0200

    Streamline build.zig

--- build.zig
+++ build.zig
@@ -4,35 +4,39 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
+    const flags = addFlags(b);
 
     const exe = b.addExecutable(.{
         .name = "cote",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+        .target = flags.target,
+        .optimize = flags.optimize,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
+    addRunStep(b, exe);
+
+    addTestStep(b, flags);
+}
+
+const Flags = struct {
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.Mode,
+};
+
+fn addFlags(b: *std.Build) Flags {
+    return Flags{
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    };
+}
+
+fn addRunStep(b: *std.Build, c: *std.Build.Step.Compile) void {
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(c);
 
     // By making the run step depend on the install step, it will be run from the
     // installation directory rather than directly from within the cache directory.
@@ -51,13 +55,15 @@ pub fn build(b: *std.Build) void {
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+}
 
+fn addTestStep(b: *std.Build, flags: Flags) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+        .target = flags.target,
+        .optimize = flags.optimize,
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
