812154852f1a14d07e75e9967aaa48bb1650156e
Micha Lehmann <michalehmann0112@gmail.com>
2024-04-19 20:53:40 +0200

    Add a bunch of debug logging

--- src/file_io.zig
+++ src/file_io.zig
@@ -1,9 +1,21 @@
 const std = @import("std");
+const builtin = @import("builtin");
+
+const log = std.log.scoped(.file_io);
 
 pub fn readFileCompletely(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
     const pwd = std.fs.cwd();
+
+    if (builtin.mode == .Debug) log.debug("Reading the entirety of file: {s}", .{try getRealPath(pwd, path)});
+
     const file = try pwd.openFile(path, .{});
     defer file.close();
 
     return file.reader().readAllAlloc(allocator, std.math.maxInt(usize));
+}
+
+fn getRealPath(dir: std.fs.Dir, path: []const u8) ![]u8 {
+    var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+
+    return try dir.realpath(path, &buf);
 }
--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,8 @@ const Component = @import("./Component.zig");
 const test_component = @import("./test_component.zig");
 const Yaml = @import("yaml").Yaml;
 
+const log = std.log;
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -13,7 +15,7 @@ pub fn main() !void {
     var components = try loadComponents(allocator);
     defer allocator.free(components);
 
-    std.log.debug("test prop: {d}", .{config.test_prop});
+    log.debug("test prop: {d}", .{config.test_prop});
 
     for (components) |component| {
         if (component.onComponentsReady) |onComponentsReady| {
@@ -23,16 +25,23 @@ pub fn main() !void {
 }
 
 fn loadConfig() !Config {
+    log.debug("Loading config.", .{});
+
     const path = "./test-config.yaml";
+    log.debug("Config path: {s}", .{path});
+
     const allocator = std.heap.page_allocator;
 
     const string = try file_io.readFileCompletely(allocator, path);
     defer allocator.free(string);
+    log.debug("Read {d} bytes.", .{string.len});
 
     return try parseConfig(allocator, string);
 }
 
 fn parseConfig(allocator: std.mem.Allocator, config_string: []u8) !Config {
+    log.debug("Parsing config.", .{});
+
     var untyped = try Yaml.load(allocator, config_string);
     defer untyped.deinit();
 
@@ -41,6 +50,8 @@ fn parseConfig(allocator: std.mem.Allocator, config_string: []u8) !Config {
 
 /// caller owns the memory returned.
 fn loadComponents(allocator: std.mem.Allocator) ![]Component {
+    log.debug("Loading components.", .{});
+
     var components = std.ArrayList(Component).init(allocator);
 
     try components.append(test_component.component);
