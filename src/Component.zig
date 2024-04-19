973d2a304fd8cb12f03adc33f5439a6a814eaf03
Micha Lehmann <michalehmann0112@gmail.com>
2024-04-20 00:17:06 +0200

    Make ABI regarding Component.name actually make sense
    Previously this was using a pointer when it shouldn't have.
    Also the length was incorrect because of the `api_version` field.
    This new size now aligns properly with no wasted space.

--- src/Component.zig
+++ src/Component.zig
@@ -1,15 +1,22 @@
 const std = @import("std");
 
+const name_len = 62;
+
 abi_version: u8,
-name: *const [63:0]u8,
+name: [name_len:0]u8,
 onComponentsReady: ?*const fn (self: *const @This()) void = null,
 
-/// pads the given name to 63 characters + sentinel.
-pub fn name(comptime original_name: [:0]const u8) *const [63:0]u8 {
-    if (original_name.len > 63) @compileError(std.fmt.comptimePrint(
-        "Names cannot be longer than 63 characters + sentinel. This name is {} characters long + sentinel.",
-        .{original_name.len},
+/// pads the given name with 0s to `name_len` characters + sentinel.
+pub fn name(comptime original_name: [:0]const u8) [name_len:0]u8 {
+    if (original_name.len > name_len) @compileError(std.fmt.comptimePrint(
+        "Names cannot be longer than {} characters + sentinel. This name is {} characters long + sentinel.",
+        .{ name_len, original_name.len },
     ));
 
-    return original_name ++ [_]u8{0} ** (63 - original_name.len);
+    comptime var padded: [name_len:0]u8 = undefined;
+
+    @memcpy(padded[0..original_name.len], original_name);
+    @memset(padded[original_name.len..], 0);
+
+    return padded;
 }
