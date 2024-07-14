c694c114b1a9e423aaaad17d7be3a3a5df0b5486
Micha Lehmann <michalehmann0112@gmail.com>
2024-07-14 10:14:09 +0200

    Upgrade to zig 0.13.0

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "cote",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = flags.target,
         .optimize = flags.optimize,
     });
@@ -68,7 +68,7 @@ fn addTestStep(b: *std.Build, flags: Flags) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = flags.target,
         .optimize = flags.optimize,
     });
