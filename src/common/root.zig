fd58f94e441db86634235feebfd7c1667d164914
Micha Lehmann <michalehmann0112@gmail.com>
2024-08-13 20:31:35 +0200

    Fix tests not running at all because fuck zig

--- src/common/root.zig
+++ src/common/root.zig
@@ -3,3 +3,11 @@ pub const ComponentName = @import("./ComponentName.zig");
 pub const Config = @import("./Config.zig");
 pub const io = @import("io.zig");
 pub const Version = @import("./Version.zig");
+
+test {
+    _ = Component;
+    _ = ComponentName;
+    _ = Config;
+    _ = io;
+    _ = Version;
+}
